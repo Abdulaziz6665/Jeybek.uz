0855571432dcdc038a7e4fe5773cc4ac3fb0868d
Abdulaziz6665 <mr.yunusobod@gmail.com>
2021-01-04 23:45:22 +0500

    tomorrow and

--- js/main.js
+++ js/main.js
@@ -1,5 +1,14 @@
 const socialBox = document.querySelector('.social-link-wrapper')
+const socialsBlock = document.querySelector('.social-wrapper')
+const btnClose = document.querySelector('.btn-close')
 
 socialBox.addEventListener('click', function (e) {
-  socialBox.classList.toggle('box-hidden')
+  socialBox.classList.add('box-hidden')
+  socialsBlock.classList.remove('social-block')
+})
+
+btnClose.addEventListener('click', function() {
+socialBox.classList.remove('box-hidden')
+socialsBlock.classList.add('social-block')
+
 })
